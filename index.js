c8d7311eaa42361dd354a03d4012ba54b113ef37
Virva <virva.svala@gmail.com>
2019-06-01 15:59:35 +0300

    setting uo heroku

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+
+// Middleware
+app.use(cors())
 
 let notes = [
   {
@@ -30,7 +35,13 @@ app.get('/notes', (req, res) => {
   res.json(notes)
 })
 
-const PORT = 3001
+// const PORT = 3001
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+//this one for heroku
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
