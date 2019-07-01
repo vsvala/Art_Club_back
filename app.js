31e56040a0a6fbfce501e3ad221c42f2e86d615f
Virva <virva.svala@gmail.com>
2019-07-02 02:29:27 +0300

    installing multer, saving uploaded image

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const imageRouter = require('./controllers/images')
 const artworksRouter = require('./controllers/artworks')
 const middleware = require('./utils/middleware.js')
 const mongoose = require('mongoose')
+//var multer = require('multer')
 
 
 // connecting to db throught config file
@@ -25,15 +27,20 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 
 
 // Middleware
+
 app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(middleware.requestLogger)
+app.use('/uploads',express.static('uploads'))//create a static path reference to folder
+
 
 app.use(cors())
 
 app.use('/api/users', usersRouter)
 app.use('/api/artworks', artworksRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/images', imageRouter)//express.static('uploads'))//statistic path for the images//, imageRouter
+//app.use(multer({ dest: 'public/uploads/',rename: function (fieldname, filename) {return filename }, }).any()) //middleware Multer to upload the photo on the server side.
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/artworks.js
+++ controllers/artworks.js
@@ -1,8 +1,39 @@
 const artworksRouter = require('express').Router()
+const multer = require('multer')
 const Artwork = require('../models/artwork')
 const User = require('../models/user')
 //const jwt = require('jsonwebtoken')
-const { authenticateToken } = require('../utils/checkRoute')
+//const { authenticateToken } = require('../utils/checkRoute')
+
+
+//multer saves image to folder
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,'./uploads/')
+    console.log('storage')
+  },
+  filename:function(req, file, cb) {
+    cb(null, Date.now() + file.originalname)
+    console.log('filename')
+  }
+})
+const fileFilter = (req, file, cb) => {
+//     //rejects storing a file
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'|| file.mimetype === 'image/pdf') {
+    cb(null,true)
+    console.log('mimetype true')
+  }else{
+    console.log('false')
+    cb(null,false)
+  }
+}
+
+const upload = multer({ storage: storage, limits:{
+  fileSize:1024*1024 *5
+},
+fileFilter:fileFilter
+})
+
 
 // artworksRouter.get('/', (req, res) => {
 //   res.send('<h1>Hello World!</h1>')
@@ -34,50 +65,65 @@ artworksRouter.get('/:id',async (req, res, next) => {
   // })
 })
 
-artworksRouter.post('/',async (req, res) => {
+artworksRouter.post('/', upload.single('galleryImage'),(req, res, next) => { //async
+  console.log('reqfile', req.file)
   const body = req.body
-  //const token = getTokenFrom(req)
+  console.log('artworkbody', body.userId)
 
+  //console.log('routerista',body.config)
+  console.log('routerista headers',req.headers)
+  console.log('routeristadata',req.file.path)
+
+  //const token = getTokenFrom(req)
   // try {
   //   const decodedToken = jwt.verify(token, process.env.SECRET)
   //   if (!token || !decodedToken.id) {
   //     return res.status(401).json({ error: 'token missing or invalid' })
-  //   }
+  //
+
+  /*   AUTORISOINTI
   const decodedToken = authenticateToken(req)
+  const user =  User.findById(decodedToken.id)//await
+ */
+  //
+  //tää pitää kai parsia
+  //const user = User.findById(body.userId)// await
+  //console.log('user_____________________________________________', user)
 
-  const user = await User.findById(decodedToken.id)
-
-
-  //const user = await User.findById(body.userId)
-  console.log('user', user)
-
-  if (body.image === undefined) {
-    return res.status(400).json({ error: 'image missing' })
-  }
-  console.log('artwork', body)
+  /*   if (body.artist === undefined) {
+    return res.status(400).json({ error: 'artist missing' })
+  } */
+  //console.log('artwork_______________________________________________________________________________________________', body)
 
   const artwork = new Artwork({
-    image: body.image,
-    artist: body.artist,
-    name: body.name,
-    year: body.year,
-    size: body.size,
-    medium:body.medium,
-    user: user._id,
+    galleryImage: req.file.path,
+    artist: req.body.artist,
+    name: req.body.name,
+    year: req.body.year,
+    size: req.body.size,
+    medium:req.body.medium,
+    user:req.body.userId
+    //user: user._id,
   })
+  artwork.save()
+  // const savedArtwork = artwork.save()//await
+  //user.artworks = user.artworks.concat(savedArtwork._id)
+  //user.save() // await
+    .then((result) => {
+      console.log(result)
+      res.status(200).json({
+        succes:true,
+        document:result
+        //res.json(savedArtwork.toJSON())
 
+        // } catch(exception) {
+        //   next(exception)
+        // }
+      })
+        .catch((err) => next(err))
 
-  const savedArtwork = await artwork.save()
-  user.artworks = user.artworks.concat(savedArtwork._id)
-  await user.save()
-
-  res.json(savedArtwork.toJSON())
-
-  // } catch(exception) {
-  //   next(exception)
-  // }
+    })
 })
-
 artworksRouter.put('/:id', (req, res, next) => {
   const body = req.body
 
@@ -88,6 +134,7 @@ artworksRouter.put('/:id', (req, res, next) => {
     year: body.year,
     size: body.size,
     medium:body.medium,
+
   }
 
   Artwork.findByIdAndUpdate(req.params.id, artwork, { new: true })
