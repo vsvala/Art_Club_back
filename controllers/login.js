c0dd941887f61c522f31395d4b8dc7eddddacec3
Virva <virva.svala@gmail.com>
2019-07-10 10:03:12 +0300

    heroku fixes/try catch for login

--- controllers/login.js
+++ controllers/login.js
@@ -5,31 +5,38 @@ const User = require('../models/user')
 
 
 
-loginRouter.post('/', async (request, response) => {
-  const body = request.body
+loginRouter.post('/', async (req, res) => {
+  const body = req.body
 
-  const user = await User.findOne({ username: body.username })
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+  try {
+    const user = await User.findOne({ username: body.username })
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
-    })
+    if (!(user && passwordCorrect)) {
+      return res.status(401).json({
+        error: 'invalid username or password'
+      })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+      role:user.role
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    res
+      .status(200)
+      .send({ token, username: user.username, name: user.name, role: user.role, id:user.id })
+  }
+  catch (error) {
+    console.log(error.message)
+    return res.status(500).json({ error: 'authentication error' })
   }
 
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-    role:user.role
-  }
-
-  const token = jwt.sign(userForToken, process.env.SECRET)
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name, role: user.role, id:user.id })
 })
 
 module.exports = loginRouter
