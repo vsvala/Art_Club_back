9853119a3838a2ad392dee39d5286d8f15a070c8
vsvala <virva.svala@gmail.com>
2026-06-27 13:20:37 +0300

    updating dependencies, and refactores code, setted up mago db again with new seeds

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
-const config = require('./utils/config')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 //Routrers
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const eventsRouter = require('./controllers/events')
-const artworksRouter = require('./controllers/artworks')
-const middleware = require('./utils/middleware.js')
-const logger = require('./utils/logger')
-const tokenCheckRouter = require('./controllers/tokenCheck')
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const eventsRouter = require("./controllers/events");
+const artworksRouter = require("./controllers/artworks");
+const middleware = require("./utils/middleware.js");
+const logger = require("./utils/logger");
+const tokenCheckRouter = require("./controllers/tokenCheck");
 
-
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 // connecting to db throught config file
 //console.log('commecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     //console.log('connected to MongoDB')
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
     //console.log('error connection to MongoDB:', error.message)
-    logger.error('error connection to MongoDB:', error.message)
-  })
-
-mongoose.set('useCreateIndex', true)
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
 // Middlewares
-app.use(cors())
-app.use(express.static('build'))
-app.use(bodyParser.json())
-app.use(middleware.requestLogger)
-app.use(express.static('build'))
-app.use('/uploads',express.static('uploads'))//create a static path reference to folder
-app.use('/public/uploads',express.static('uploads'))
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(express.static("build"));
+app.use("/uploads", express.static("uploads")); //create a static path reference to folder
+app.use("/public/uploads", express.static("uploads"));
 
+const apiUrl = "/api";
+app.use(`${apiUrl}/users`, usersRouter);
+app.use(`${apiUrl}/artworks`, artworksRouter);
+app.use(`${apiUrl}/events`, eventsRouter);
+app.use(`${apiUrl}/login`, loginRouter);
+app.use(`${apiUrl}/tokenCheck`, tokenCheckRouter);
 
-const apiUrl = '/api'
-app.use(`${apiUrl}/users`, usersRouter)
-app.use(`${apiUrl}/artworks`, artworksRouter)
-app.use(`${apiUrl}/events`, eventsRouter)
-app.use(`${apiUrl}/login`, loginRouter)
-app.use(`${apiUrl}/tokenCheck`, tokenCheckRouter)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-module.exports = app
+module.exports = app;
