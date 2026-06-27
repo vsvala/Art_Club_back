fbabec23e41f223850f856ff29b289f3efb0d4a7
vsvala <virva.svala@gmail.com>
2026-06-27 16:51:06 +0300

    image upload to use cloudinary

--- utils/config.js
+++ utils/config.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
+require("dotenv").config();
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+let PORT = process.env.PORT;
+let MONGODB_URI = process.env.MONGODB_URI;
 
 /* if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
  */
-let SECRET = process.env.SECRET
+let SECRET = process.env.SECRET;
 
 module.exports = {
   MONGODB_URI,
   PORT,
-  SECRET
-}
+  SECRET,
+};
