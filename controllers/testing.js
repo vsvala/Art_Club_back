fc6821d77d8500a4177f1504e0c127ae341436d9
vsvala <virva.svala@gmail.com>
2026-07-10 19:17:05 +0300

    fix(testing): add try/catch to all testing controller handlers

--- controllers/testing.js
+++ controllers/testing.js
@@ -3,34 +3,46 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const Artwork = require("../models/artwork");
 
-router.post("/reset", async (req, res) => {
-  await Artwork.deleteMany({});
-  await User.deleteMany({});
-  res.status(204).end();
+router.post("/reset", async (req, res, next) => {
+  try {
+    await Artwork.deleteMany({});
+    await User.deleteMany({});
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/users", async (req, res) => {
-  const { name, email, username, password, role } = req.body;
-  const passwordHash = await bcrypt.hash(password, 10);
-  const user = new User({ name, email, username, passwordHash, role });
-  const savedUser = await user.save();
-  res.status(201).json(savedUser.toJSON());
+  try {
+    const { name, email, username, password, role } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, username, passwordHash, role });
+    const savedUser = await user.save();
+    res.status(201).json(savedUser.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/artworks", async (req, res) => {
-  const { galleryImage, artist, name, year, size, medium, likes, user } = req.body;
-  const artwork = new Artwork({
-    galleryImage,
-    artist,
-    name,
-    year,
-    size,
-    medium,
-    likes: likes ? likes : 0,
-    user,
-  });
-  const savedArtwork = await artwork.save();
-  res.status(201).json(savedArtwork.toJSON());
+router.post("/artworks", async (req, res, next) => {
+  try {
+    const { galleryImage, artist, name, year, size, medium, likes, user } = req.body;
+    const artwork = new Artwork({
+      galleryImage,
+      artist,
+      name,
+      year,
+      size,
+      medium,
+      likes: likes ? likes : 0,
+      user,
+    });
+    const savedArtwork = await artwork.save();
+    res.status(201).json(savedArtwork.toJSON());
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
