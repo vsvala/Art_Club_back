0c476792c97c0d8aaba4f53ffb3355adaf658dcf
vsvala <virva.svala@gmail.com>
2026-07-16 22:17:31 +0300

    fix(instrument): add missing trace samplerate setting

--- instrument.js
+++ instrument.js
@@ -3,6 +3,7 @@ const Sentry = require("@sentry/node");
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   environment: process.env.NODE_ENV || "production",
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1.0,
   dataCollection: {
     // Configure privacy defaults by uncommenting these lines if needed.
     // userInfo: false,
